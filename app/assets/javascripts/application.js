58a4a7aded9e73933a1fc8d182a64433e982b266
Michael Lampe <mgoldhand@googlemail.com>
2012-06-15 20:51:21 +0200

    arbeiten am karten erstellen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,31 +1,8 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require_tree .
-
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.all
+
 var map;
 var markerzwischenspeicher = new Array;
 var markerscount = 0;
@@ -33,72 +10,87 @@ var mymarker;
 var markers = new Array;
 var markernr;
 var marker;
-var markername = ""
+var markername = "";
 var infowindow = new google.maps.InfoWindow();
 var kategorie;
 var kates = "";
-google.maps.event.addListener(infowindow,'closeclick',function(){
-  if(marker.getTitle()==null){
-  infowindow.setContent(infowindowcontent);
-  infowindow.open(map,marker);
-  }
-});
 
 
 function initialize() {
+  //google map optionen setzen
   var myOptions = {
     zoom: 14,
     center: new google.maps.LatLng(53.566822,10.005798),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoomControl: true,
-    zoomControlOptions: {
+    zoomControlOptions:{
         style: google.maps.ZoomControlStyle.LARGE,
         position: google.maps.ControlPosition.RIGHT_BOTTOM
     },
     disableDefaultUI:true
-  }
+  };
+  // map erstellen
   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-}
+};
 
-function initmapsfunctions(kate){
-  kategorie = kate
+// map funktionen fuer das editieren und erstellen von markern
+function initmapsfunctions(kategorie){
     $(document).ready(function(){
-      $.each(kategorie,function( intIndex, objValue ){ kates += '<option value="'+objValue+'">'+objValue+'</option>'});
-      $('#form1').append('<select id="katesmap">'+ kates +'</select>');
-      google.maps.event.addListener(window.map, 'click', function(event) {
-        infowindow.close();
-        addMarker(event.latLng);});
+      kategoriedropdownmenu(kategorie);
+      mapeventlistern();
     });
 }
+
+// kategorien aus rails uebergebn und in ein dropdown menu uerbegeben
+function kategoriedropdownmenu(kategorie){
+ $.each(kategorie,function( intIndex, objValue ){ kates += '<option value="'+objValue+'">'+objValue+'</option>'});
+}
+
+function mapeventlistern(){
+  google.maps.event.addListener(window.map, 'click', function(event) {
+    infowindow.close();
+    addMarker(event.latLng);
+  });
+  google.maps.event.addListener(infowindow,'closeclick',function(){
+    if(marker.getTitle()==null){
+    infowindow.setContent(infowindowcontent);
+    infowindow.open(map,marker);
+    }
+  });
+  google.maps.event.addListener(marker, 'dblclick', function(event) {
+    this.setMap(null);
+    $('#'+markername).remove()
+    markerzwischenspeicher.splice(markerzwischenspeicher.indexOf(marker),1);
+  });
+}
+
 function addMarker(location) {
     markername = "marker"+markerscount
     marker = new google.maps.Marker({
-    position: location,
-    map: map,
-    draggable: false,
-    visible: true
-  });
+      position: location,
+      map: map,
+      draggable: false,
+      visible: true
+     });
   $('#directionsPanel').append('<div id=marker'+markerscount+'></div>');
   infowindowcontent = '<div id="titleform" >'+
                         '<form> Title: <br><input id="'+markername+'form" name="title" type="text" size="30" maxlength="30">'+
                         '<select id="kates">'+ kates +'</select>'+
-                        '<input style="display:block" type="button" name="setmarkertitle" value="Naam" onclick="setMarkerTitle(marker)">'+
+                        '<input style="display:block" type="button" id="setmarkertitle" value="Speichern" onclick="setMarkerTitle(marker)">'+
                         '</form></div>';
   infowindow.setContent(infowindowcontent);
   infowindow.open(map,marker);
-  google.maps.event.addListener(marker, 'dblclick', function(event) {
-    this.setMap(null);
-   $('#'+markername).remove()
-    markerzwischenspeicher.splice(markerzwischenspeicher.indexOf(marker),1);
-  });
   markerzwischenspeicher.push(marker)
   markerscount +=1;
 }
 
+
+
+
 function setMarkerTitle(marker){
-  $('#'+markername).append($('#'+markername+'form').val());
   marker.setTitle($('#'+markername+'form').val());
-  $('#'+markername).append('<div id=kat'+markername+'>' + $('#kates').val() + '</div>');
+  $('#sortable').append('<li id=kat'+markername+'>'+ $('#'+markername+'form').val() + $('#kates').val() + '</li>');
+  $('#setmarkertitle').attr('onclick','').unbind('click');
 }
 
 function jsonfiemarker(marker){
@@ -136,6 +128,17 @@ function savemap(){
   });
 };
 
+
+
+
+
+
+
+
+
+
+
+
 function showmap(marker){
   var data = JSON.parse(marker);
   $("#content").append("<div id='mardisplay'></div>")
@@ -161,6 +164,12 @@ function showmap(marker){
   });
 }
 
+
+
+
+
+
+//Suche 
 var addressField = document.getElementById('search_address');
 var geocoder = new google.maps.Geocoder();
 function search() {
