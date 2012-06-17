f73e356e0d382c346c59ed6e6638f5f171361e06
micha <micha@micha-EP43-UD3L.(none)>
2012-06-18 01:28:00 +0200

    create map update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,16 @@
 //= require_tree .
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery.ui.all
 
 
 
 var map;
 var kates = "";
+var comments = ""
+var markers = new Array(); 
 var marker;
 var markerscount = 0;
-
 var bool="true";
 var markername = "";
 var infoboxoptions = {
@@ -23,8 +24,6 @@ var infoboxoptions = {
                   ,opacity: 0.90
                   ,width: "280px"
                  }
-                ,closeBoxMargin: "0px 0px 0px 0px"
-                ,closeBoxURL: ""
                 ,infoBoxClearance: new google.maps.Size(1, 1)
                 ,isHidden: false
                 ,pane: "floatPane"
@@ -58,6 +57,11 @@ function mapeventlistern(){
     google.maps.event.addListener(map, 'click', function(event) {
       checkmarkertitle(event.latLng)
     });
+    google.maps.event.addListener(infowindow, 'closeclick',function(event){
+      marker.setMap(null);
+      bool="true";
+    });
+
 }
 function addMarker(location) {
   markername = "marker"+markerscount
@@ -69,22 +73,52 @@ function addMarker(location) {
      });
   infowindowcontent = '<div id="titleform">'+
                       '<form> Title: <br><input id="'+markername+'form" name="title" type="text" size="30" maxlength="30">'+kates+
-                      '<input style="display:block;z-index:150" type="button" id="setmarkertitle" value="Speichern" onclick="setMarkerTitle(marker)">'+
+                      '<textarea id="comments'+markername+'" name="comments" cols="30" rows="5">Enter your comments here...</textarea><br>'+
+                      '<input style="display:block;z-index:150" type="button" id="setmarkertitle" value="Speichern" onclick="setMarkerTitle('+markerscount+')">'+
                       '</form></div>';
   infowindow.setContent(infowindowcontent);
   infowindow.open(map,marker);
-    bool="false";
+  google.maps.event.addListener(marker, 'dblclick', function(event) {
+    alert("test")
+  });
+  markers.push(marker);
+  bool="false";
   markerscount+=1
 }
 
 
+//save the marker
+function jsonfiemarker(marker){
+  for(var i=0,len=marker.length; value=marker[i], i<len; i++) {
+    var werte='{"title": "'+ value.getTitle() +'", "lat": "'+ value.getPosition().lat()+'","lng": "'+value.getPosition().lng()+'","kategorie": "'+ $('#katmarker'+i).text().toString() +'"}';
+    var js1 = JSON.parse(werte);
+    var str ='"marker'+i+'" :' + werte;
+    markers.push(str);
+  };
+  mymarker='{"markers":{' + markers + '},"katesmap":"'+ $('#katesmap').val() +'", "mapname":"'+ $('#mapname').val() +'"}'
+  mymarker= JSON.parse(mymarker);
+};
+
+function savemap(){
+  $(document).ready(function() {   
+    $("#saveme").click(function () {
+      $('.group').each(function(index) {
+        alert($(this).find("h3").find("input").val());
+        alert($(this).find("div").find("p").find("select").val());
+        alert($(this).find("div").find("textarea").val());
+      });
+    });
+  });
+};
 
 //////helper methoden//////
 
 // kategorien aus rails uebergebn und in ein dropdown menu uerbegeben
+
 function kategoriedropdownmenu(kategorie){
- $.each(kategorie,function( intIndex, objValue ){ kates += '<option value="'+objValue+'">'+objValue+'</option>'});
-  kates += '<select id="kates">'+ kates +'</select>'
+  k = "";
+  $.each(kategorie,function( intIndex, objValue ){ k += '<option value="'+objValue+'">'+objValue+'</option>'});
+  kates += '<select id="kates">'+ k +'</select>'
 }
 
 function checkmarkertitle(el){
@@ -101,9 +135,38 @@ function checkmarkertitle(el){
       }
 }
 
-function setMarkerTitle(marker){
+function setMarkerTitle(markerid){
+  kat = $('#kates').val()
+  if ($('#'+markername+'form').val()==""){
+        $(function() {
+          $( "#dialog-message" ).dialog({
+            modal: true,
+            buttons: {Ok: function(){$( this ).dialog( "close" );}},
+            closeText: 'hide'
+          });
+        });
+  }else{
   marker.setTitle($('#'+markername+'form').val());
-  $('#markerlist').append('<li id=kat'+markername+'>'+ $('#'+markername+'form').val() + $('#kates').val() + '</li>');
+  kates2 = kates
+  $('#markerlist').append('<div class="group" id=kat'+markername+' >'+
+                          '<h3><a href="#"><input id="markertitle" name="title" type="text" size="30" maxlength="30" value="'+ $('#'+markername+'form').val()+'"></a></h3><div>'+
+                          '<p>'+ kates +'</p>'+
+                          '<textarea id="comments" name="comments" cols="30" rows="5">'+ $('#comments'+markername).val()+'</textarea><br>'+
+                          '<input style="display:block;z-index:150" type="button" id="deletmarker" value="Loeschen" onclick="deletemarker(kat'+markername+','+markerid +')">'+
+                          '</div></div>'
+                          ).accordion('destroy').accordion({header: "h3",collapsible: true, active: false }).sortable({header:"h3"});
   $('#setmarkertitle').attr('onclick','').unbind('click');
+  $('#setmarkertitle').css("color","grey");
   bool="true";
+  $('#kates').attr("id","kates"+markername);
+  $('#kates'+markername).val(kat)
+  setTimeout(function(){infowindow.close(map,marker);},50);
+  };
+}
+
+function deletemarker(name,markerid){
+  alert(markerid)
+  markers[markerid].setMap(null);
+  $(name).remove()
+  $('#markerlist').accordion('destroy').accordion({header: "h3",collapsible: true, active: false }).sortable({header:"h3"});
 }
